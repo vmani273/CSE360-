4d33a4346693e677cf686fba1853e8306d065d17
Vishwas Mani <vishwasmani@gmail.com>
2020-10-07 17:39:12 -0700

    initial version

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,45 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	
+	/**
+	 * Represent a machine which can perform several actions
+	 * such as adding, subtracting, converting to a string, etc.
+	 */
+	
+	  private int total;
+	  
+	  public AddingMachine () { 
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /*
+	   * return the current total
+	   */
+	  public int getTotal () { 
+	    return 0;
+	  }
+	  
+	  /*
+	   * adds the parameter to the total variable
+	   */
+	  public void add (int value) {
+		  
+	  }
+	  
+	  /*
+	   * subtracts the parameter from the total variable
+	   */
+	  public void subtract (int value) {
+	  }
+	
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  /*
+	   * clears the AddingMachine.
+	   */
+	  public void clear() {
+	  }
+}
